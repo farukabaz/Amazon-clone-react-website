e78417a9cbdbff0b719fc740860fc2eb0dda1afc
Faruk Abaz <35192754+farukabaz@users.noreply.github.com>
2020-11-03 09:51:55 +0100

    Started checkout page

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search"
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div className='header'>
 
+            <Link to='/'>
             <img className="header__logo" src="https://www.mabaya.com/wp-content/uploads/2019/10/amazon_PNG25.png" />
+            </Link>
             
             <div className="header__search">
                 <input className="header__searchInput" type="text"/>
@@ -40,10 +43,12 @@ function Header() {
                     </span>
                 </div>
                 
-                <div className='header__optionBasket'>
-                    <ShoppingBasketIcon />
-                    <span className='header__optionLineTwo header__basketCount'>0</span>
-                </div>
+                <Link to='/checkout'>
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='header__optionLineTwo header__basketCount'>0</span>
+                    </div>
+                </Link>
 
             </div> 
         </div>
